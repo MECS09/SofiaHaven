441f248e58c83f8f5426906d8e39f934cb557e46
melanie marie flores <melaniemarieflores@gmail.com>
2020-10-05 02:53:22 +0800

    setup featured book slider

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -5,31 +5,122 @@
     @endsection
 
     <div class="container-fluid">
-        <div class="owl-carousel owl-theme">
-            <div class="item"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(45).jpg"
-                alt="First slide"></div>
-            <div class="item"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(46).jpg"
-                alt="Second slide"></div>
-            <div class="item"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(47).jpg"
-                alt="Third slide"></div>
-                <div class="item"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(45).jpg"
-                    alt="First slide"></div>
-                <div class="item"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(46).jpg"
-                    alt="Second slide"></div>
-                <div class="item"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(47).jpg"
-                    alt="Third slide"></div>
-                    <div class="item"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(45).jpg"
-                        alt="First slide"></div>
-                    <div class="item"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(46).jpg"
-                        alt="Second slide"></div>
-                    <div class="item"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(47).jpg"
-                        alt="Third slide"></div>
-                        <div class="item"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(45).jpg"
-                            alt="First slide"></div>
-                        <div class="item"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(46).jpg"
-                            alt="Second slide"></div>
-                        <div class="item"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(47).jpg"
-                            alt="Third slide"></div>
+        <div class="slider-featured py-5">
+            <!--Carousel Wrapper-->
+            <div id="carousel-example-1z" class="carousel slide carousel-slide" data-ride="carousel">
+                <!--Indicators-->
+                <ol class="carousel-indicators">
+                <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-1z" data-slide-to="1"></li>
+                <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+                </ol>
+                <!--/.Indicators-->
+                <!--Slides-->
+                <div class="carousel-inner" role="listbox">
+                <!--First slide-->
+                <div class="carousel-item active">
+                   <div class="row justify-content-md-center">
+                       <div class="col-sm-3">
+                        <img class="d-block w-100" src="{{asset('')}}img/book cover/placeholder1.jpg"
+                        alt="First slide">
+                       </div>
+                       <div class="col-sm-6">
+                           <h3 class="text-center">
+                               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur, rerum? Doloremque voluptates quisquam amet quo veritatis fugiat architecto repellat. Pariatur assumenda ex temporibus saepe ea, illo placeat! Quaerat, obcaecati odio.
+                           </h3>
+                       </div>
+                   </div>
+                </div>
+                <!--/First slide-->
+                <!--Second slide-->
+                <div class="carousel-item">
+                    <div class="row justify-content-md-center">
+                        <div class="col-sm-3">
+                         <img class="d-block w-100" src="{{asset('')}}img/book cover/placeholder1.jpg"
+                         alt="First slide">
+                        </div>
+                        <div class="col-sm-6">
+                            <h3 class="text-center">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae excepturi natus incidunt, molestias impedit officiis quis dolorem reprehenderit amet exercitationem, delectus rerum non. Ipsum ullam veritatis, nobis cupiditate minima recusandae.
+                            </h3>
+                        </div>
+                    </div>
+                 </div>
+                <!--/Second slide-->
+                <!--Third slide-->
+                <div class="carousel-item">
+                    <div class="row justify-content-md-center">
+                        <div class="col-sm-3">
+                         <img class="d-block w-100" src="{{asset('')}}img/book cover/placeholder1.jpg"
+                         alt="First slide">
+                        </div>
+                        <div class="col-sm-6">
+                            <h3 class="text-center">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, repudiandae quam asperiores vel numquam id dolor eos aliquid libero adipisci, soluta magnam nemo expedita voluptatibus dolorum animi velit minus distinctio?
+                            </h3>
+                        </div>
+                    </div>
+                 </div>
+                <!--/Third slide-->
+                </div>
+                <!--/.Slides-->
+                <!--Controls-->
+                <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+                </a>
+                <!--/.Controls-->
+            </div>
+            <!--/.Carousel Wrapper-->
+        </div>
+
+
+
+        <div class="slider-section p-5">
+            <div class="row justify-content-md-center">
+                <div class="col-sm-8">
+                    <div class="owl-carousel owl-theme">
+                        <div class="item">
+                            <img class="d-block w-100" src="{{asset('')}}img/book cover/placeholder1.jpg"
+                            alt="First slide">
+                        </div>
+                        <div class="item">
+                            <img class="d-block w-100" src="{{asset('')}}img/book cover/placeholder1.jpg"
+                            alt="First slide">
+                        </div>
+                        <div class="item">
+                            <img class="d-block w-100" src="{{asset('')}}img/book cover/placeholder1.jpg"
+                            alt="First slide">
+                        </div>
+                        <div class="item">
+                            <img class="d-block w-100" src="{{asset('')}}img/book cover/placeholder1.jpg"
+                            alt="First slide">
+                        </div>
+                        <div class="item">
+                            <img class="d-block w-100" src="{{asset('')}}img/book cover/placeholder1.jpg"
+                            alt="First slide">
+                        </div>
+                        <div class="item">
+                            <img class="d-block w-100" src="{{asset('')}}img/book cover/placeholder1.jpg"
+                            alt="First slide">
+                        </div>
+                        <div class="item">
+                            <img class="d-block w-100" src="{{asset('')}}img/book cover/placeholder1.jpg"
+                            alt="First slide">
+                        </div>
+                        <div class="item">
+                            <img class="d-block w-100" src="{{asset('')}}img/book cover/placeholder1.jpg"
+                            alt="First slide">
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            
         </div>
     </div>
     
@@ -41,6 +132,7 @@
                 loop:true,
                 margin:10,
                 nav:true,
+                navText:["<div class='nav-btn prev-slide'><i class='fas fa-angle-left'></i></div>","<div class='nav-btn next-slide'><i class='fas fa-angle-right'></i></div>"],
                 responsive:{
                     0:{
                         items:1
--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,8 +1,10 @@
 {{-- styles, meta,head script --}}
 
 <link rel="stylesheet" href="{{asset('')}}css/main.css">
+<link rel="stylesheet" href="{{asset('')}}plugins/fontawesome/css/all.css">
 <link rel="stylesheet" href="{{asset('')}}css/bootstrap.css">
 <link rel="stylesheet" href="{{asset('')}}css/mdb.css">
+
 
 <!-- jQuery -->
 <script type="text/javascript" src="{{asset('')}}js/jquery.min.js"></script>
