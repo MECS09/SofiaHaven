ede773b72ebd55fc5f45700f2880c82d1652d548
Mar eli <mesagsagat@gmail.com>
2020-11-05 15:18:42 +0800

    update maintenance page version 1

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -1,9 +1,97 @@
-<!-- simple maintenance signage-->
-<marquee>
-<h2 style="color:red;margin-top:100">
-SofiaHaven Website is under construction.
-</h2>
-</marquee>
-<div style="margin:0 auto;background-image:url('{{asset('')}}img/maintenance/sofiahavenhomepage.png');height:400px;width:800px;background-size:cover;">
+@include('inc.header')
+<style>
 
-</div>
+body,html{
+    height:100%;
+}
+
+#intro {
+    background: url("{{asset('')}}img/maintenance-background.jpg")no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+
+
+  /* Add the blur effect */
+  filter: blur(8px);
+  -webkit-filter: blur(8px);
+
+  /* Full height */
+  height: 100%;
+
+    
+}
+.sample{
+    z-index:2;
+    color: white;
+    font-weight: bold;
+    border: 3px solid #f1f1f1;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 2;
+    width: 80%;
+    padding: 20px;
+    text-align: center;
+}
+</style>
+        <!--Mask-->
+        <div id="intro" class="view">
+        </div>
+            <div class="mask rgba-black-strong sample">
+
+                <div class="container-fluid d-flex align-items-center justify-content-center h-100">
+
+                    <div class="row d-flex justify-content-center text-center">
+
+                        <div class="col-md-10 ">
+
+                            <!-- Heading -->
+                            <h2 class="display-4 font-weight-bold white-text pt-5 mb-2" style="font-family:afternoon;">SofiaHaven</h2>
+
+                            <!-- Divider -->
+                            <hr class="hr-light">
+
+                            <!-- Description -->
+                            <h4 class="white-text my-4">Please be informed that SofiaHaven Website is under maintenance.</h4>
+                            <button type="button" class="btn btn-outline-white" data-toggle="modal" data-target="#basicExampleModal">Want to donate a coffee?<i class="fa fa-coffee ml-2"></i> Click here!</button>
+                                
+        
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+<!-- Modal -->
+                                <div class="modal fade" id="basicExampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                                aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel"><b>Gcash Account Detail:</b></h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Name: Sofia Haven<br>
+                                        Number: 09999999999<br>
+                                        Amount: 1000.00php<br>
+
+
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-light" data-dismiss="modal">Close</button>
+                                    </div>
+                                    </div>
+                                </div>
+                                </div>
+
+
+@include('inc.scripts')
+
