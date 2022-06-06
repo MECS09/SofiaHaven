b44b12a2778f44d833b797d4d30ed02e92b734bd
melanie marie flores <melaniemarieflores@gmail.com>
2022-06-06 15:52:54 +0800

    blog update

--- resources/views/guest/blog-single.blade.php
+++ resources/views/guest/blog-single.blade.php
@@ -31,18 +31,20 @@
                         </div>
                         <div class="row justify-content-center relative">
                             <div class="col-sm-4 text-center">
+                                @auth
                                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                      <img class="blog-author-thumb px-1" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->fist_name }}" />
-                                    
-                                @else
-                                    <div>{{ Auth::user()->first_name }}</div>
-                                        <div class="ml-1">
-                                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                            </svg>
-                                            <i class="fas fa-user fa-lg"></i>
-                                        </div>
-                                @endif
+                                            <img class="blog-author-thumb px-1" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->fist_name }}" />
+                                        
+                                    @else
+                                        <div>{{ Auth::user()->first_name }}</div>
+                                            <div class="ml-1">
+                                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                                </svg>
+                                                <i class="fas fa-user fa-lg"></i>
+                                            </div>
+                                    @endif
+                                @endauth
                                 <a href="#">
                                    
                                     Sofia's Haven
